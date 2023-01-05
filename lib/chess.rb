d4503c1d52da1d11dee35915a323770608bc71e2
Clipporion <marcusberger008@gmx.net>
2023-01-05 15:57:58 +0100

    Add possibility to save

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,9 +3,13 @@
 require_relative 'board'
 require_relative 'pieces'
 require_relative 'node'
+require_relative 'rochade'
+require 'yaml'
+require 'psych'
 
 # This is the class where all the input and game logic will be stored.
 class Game
+  include Rochade
   attr_accessor :board, :turn, :current_color, :white_pieces, :black_pieces, :check_mate
   attr_reader :white_king, :black_king
 
@@ -18,7 +22,6 @@ class Game
     @white_king = @board.board[['e', 1]].piece
     @white_pieces = fill_pieces('white')
     @black_pieces = fill_pieces('black')
-    @check_mate = false
   end
 
   def fill_pieces(color)
@@ -29,16 +32,28 @@ class Game
     res
   end
 
-  def user_input
-    puts "#{@current_color.capitalize} player, please enter start field and target field:"
-    input = gets.chomp
+  def save_game
+    Dir.mkdir('savegame') unless Dir.exist?('savegame')
+    File.open('./savegame/savegame.yaml', 'w') { |file| file.write(YAML.dump(self)) }
+    @check_mate = true
+  end
+
+  def user_input(input = gets.chomp)
     if input.include?(' ')
-      start = cleanup_input(input.split[0].split(''))
-      target = cleanup_input(input.split[1].split(''))
-      [start, target]
+      clean_input(input)
+    elsif input == 'save'
+      save_game
+    elsif %w[short long].include?(input)
+      input
     else
       user_input
     end
+  end
+
+  def clean_input(input)
+    start = cleanup_input(input.split[0].split(''))
+    target = cleanup_input(input.split[1].split(''))
+    [start, target]
   end
 
   def cleanup_input(input)
@@ -75,16 +90,16 @@ class Game
     false
   end
 
-  def move_piece(start, target, target_piece = @board.board[target].piece, start_piece = @board.board[start].piece)
+  def move_piece(start, target, mod, target_piece = @board.board[target].piece, start_piece = @board.board[start].piece)
     update_board(start_piece, target_piece)
     kings_in_check
     unless (@current_color == 'white' && !@white_king.is_checked) ||
            (@current_color == 'black' && !@black_king.is_checked)
-      reset(start, target, start_piece, target_piece)
+      reset(start, target, start_piece, target_piece, mod)
     end
   end
 
-  def reset(start, target, start_piece, target_piece)
+  def reset(start, target, start_piece, target_piece, mode)
     puts "#{start_piece.color.capitalize} player, this move would put your king in check!"
     target_piece.location = target
     @board.board[target].piece = target_piece
@@ -92,7 +107,7 @@ class Game
     @board.board[start].piece = start_piece
     @white_pieces.push(target_piece) if target_piece.color == 'white'
     @black_pieces.push(target_piece) if target_piece.color == 'black'
-    player_turn
+    player_turn unless mode == 'r'
   end
 
   def update_board(start_piece, target_piece)
@@ -131,8 +146,6 @@ class Game
       end
       remove_king_move(king, move_line)
     end
-    p king.possible_moves
-    p king.attacking_lines
   end
 
   def remove_king_move(king, move_line)
@@ -151,9 +164,14 @@ class Game
     @current_color = 'white'
   end
 
-  def player_turn(input = user_input, start = input[0], target = input[1])
+  def player_turn(input = user_input, mode = '')
+    return if input == 'save'
+    return rochade(input) if %w[long short].include?(input)
+
+    start = input[0]
+    target = input[1]
     if everything_valid?(start, target)
-      move_piece(start, target)
+      move_piece(start, target, mode)
     else
       puts 'Incorrect input'
       player_turn
@@ -196,7 +214,109 @@ class Game
     false
   end
 
+  # def rochade(input)
+  #   king = current_color == 'white' ? @white_king : @black_king
+  #   rook = gather_rook(input)
+
+  #   if rook.is_a?(Rook) && !king.was_moved && !rook.was_moved
+  #     check_rochade(king, rook, input)
+  #   else
+  #     puts "#{input.capitalize} Rochade not possible"
+  #     player_turn
+  #   end
+  # end
+
+  # def gather_rook(input)
+  #   case input
+  #   when 'short'
+  #     rook = current_color == 'white' ? @board.board[['h', 1]].piece : @board.board[['h', 8]].piece
+  #   when 'long'
+  #     rook = current_color == 'white' ? @board.board[['a', 1]].piece : @board.board[['a', 8]].piece
+  #   end
+  #   rook
+  # end
+
+  # def check_rochade(king, rook, input)
+  #   if input == 'short'
+  #     short_rochade_move(king, rook, input)
+  #   else
+  #     long_rochade_move(king, rook, input)
+  #   end
+  # end
+
+  # def short_rochade_move(king, rook, input)
+  #   if @current_color == 'white' && check_white_fields_short
+  #     move_king_short_white(king, rook)
+  #   elsif @current_color == 'black' && check_black_fields_short
+  #     move_king_short_black(king, rook)
+  #   else
+  #     puts "#{input.capitalize} Rochade not possible, a piece is in the way."
+  #     player_turn
+  #   end
+  # end
+
+  # def check_white_fields_short
+  #   return true if @board.board[['f', 1]].piece.color == 'none' && @board.board[['g', 1]].piece.color == 'none'
+
+  #   false
+  # end
+
+  # def check_black_fields_short
+  #   return true if @board.board[['f', 8]].piece.color == 'none' && @board.board[['g', 8]].piece.color == 'none'
+
+  #   false
+  # end
+
+  # def move_king_short_white(king, rook)
+  #   player_turn(user_input('e1 f1'), 'r')
+  #   player_turn(user_input('f1 g1'), 'r') if king.location == ['f', 1]
+  #   if king.location == ['g', 1]
+  #     move_rook_short(rook)
+  #   else
+  #     reset_king(king)
+  #     player_turn
+  #   end
+  # end
+
+  # def move_king_short_black(king, rook)
+  #   player_turn(user_input('e8 f8'), 'r')
+  #   player_turn(user_input('f8 g8'), 'r') if king.location == ['f', 8]
+  #   if king.location == ['g', 8]
+  #     move_rook_short(rook)
+  #   else
+  #     reset_king(king)
+  #     player_turn
+  #   end
+  # end
+
+  # def reset_king(king, start = @board.board[king.location])
+  #   case @current_color
+  #   when 'white'
+  #     start.piece = Empty.new(king.location)
+  #     @board.board[['e', 1]].piece = king
+  #     king.location = ['e', 1]
+  #   when 'black'
+  #     start.piece = Empty.new(rook.location)
+  #     @board.board[['e', 8]].piece = king
+  #     king.location = ['e', 8]
+  #   end
+  # end
+
+  # def move_rook_short(rook, start = @board.board[rook.location])
+  #   case @current_color
+  #   when 'white'
+  #     start.piece = Empty.new(rook.location)
+  #     @board.board[['f', 1]].piece = rook
+  #     rook.location = ['f', 1]
+  #   when 'black'
+  #     start.piece = Empty.new(rook.location)
+  #     @board.board[['f', 8]].piece = rook
+  #     rook.location = ['f', 8]
+  #   end
+  # end
+
   def play_round
+    puts "#{@current_color.capitalize} player, please enter start field and target field or long/short for a rochade:"
     player_turn
     system('clear')
     @board.display
@@ -206,8 +326,12 @@ class Game
   end
 end
 
+puts 'Welcome to chess, please enter 1 to start a new game or 2 to load a saved game:'
+input = gets.chomp.to_i until (1..2).include?(input)
+
+game = input == 1 ? Game.new : Psych.unsafe_load(File.open('./savegame/savegame.yaml', 'r'))
+
 system('clear')
-game = Game.new
 game.board.display
 
 game.play_round until game.check_mate
